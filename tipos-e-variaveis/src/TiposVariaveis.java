2a27f7e00c19b4aae3680345096607c7f7136da4
Lélia Salles <89154346+leliasalles@users.noreply.github.com>
2024-06-14 16:33:12 -0300

    Added description of priitive data types

--- tipos-e-variaveis/src/TiposVariaveis.java
+++ tipos-e-variaveis/src/TiposVariaveis.java
@@ -1,8 +1,17 @@
 public class TiposVariaveis{
-
+/**
+ * Constantes são escritas em Upper Case (Caixa Alta) e precisam
+ * ter a palavra reservada final antes do tipo de dado
+ * Constants must be written in UpperCase and have to have the 
+ * reserved word final before the data type
+ */
    public static void main(String[] args) throws Exception{
 
-    System.out.println("Hello, world!");
+    final String NAME = "Lelia Salles";
+
+    System.out.println("Hello, world! My name is :" + NAME);
 
    }
+
+
 }
